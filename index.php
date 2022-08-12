533308a000e5d7554ddc3f6e08d64b5a5a178bfe
XzS99 <75234260+XzS99@users.noreply.github.com>
2022-08-12 16:25:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ip address checker</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <style>
+        body, h1 {
+            font-family: "Raleway", sans-serif
+        }
+
+        body, html {
+            height: 100%
+        }
+
+        .bg-img {
+            background-image: url('1.webp');
+            min-height: 100%;
+            background-position: center;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+<div class="bg-img w3-display-container w3-animate-opacity w3-text-white">
+    <div class="w3-display-top-left w3-padding-large w3-large">
+        IP address checker
+    </div>
+    <div class="w3-display-middle">
+        <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
+        <hr class="border-grey" style="margin:auto;width:40%">
+        <p class="w3-large w3-center">35 days left</p>
+    </div>
+    <div class="display-bottom left padding-large">
+    </div>
+</div>
+</body>
+</html>
