9a688c0a4b17a0982bf83f2c38753d0a8865a610
XzS99 <75234260+XzS99@users.noreply.github.com>
2022-08-12 18:41:48 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,34 @@
-<!DOCTYPE html>
+<?php
+$input = $_POST['ip'];
+echo '<div class="main-container">';
+if (filter_var($input, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== FALSE) {
+    echo '<span class="result-container">' . $input . '<br>' . " Is a Valid IPv4" . '</span>';
+} elseif
+(filter_var($input, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== FALSE) {
+    echo '<span class="result-container"> ' . $input . '<br>' . " Is a Valid IPv6" . '</span>';
+} else {
+    echo "Invalid Ip address";
+}
+echo '</div>';
+?>
 <html lang="en">
 <head>
-    <title>Ip address checker</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <style>
-        body, h1 {
-            font-family: "Raleway", sans-serif
+        @import url(https://fonts.googleapis.com/css?family=Lato:300);
+
+        .main-container {
+            text-align: center
         }
 
-        body, html {
-            height: 100%
+        body {
+            background-color: #4d5057;
         }
 
-        .bg-img {
-            background-image: url('1.webp');
-            min-height: 100%;
-            background-position: center;
-            background-size: cover;
+        .result-container {
+            color: #4ea5d9;
+            font-size: 50px
         }
     </style>
+    <title>Check</title>
 </head>
-<body>
-<div class="bg-img w3-display-container w3-animate-opacity w3-text-white">
-    <div class="w3-display-top-left w3-padding-large w3-large">
-        IP address checker
-    </div>
-    <div class="w3-display-middle">
-        <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
-        <hr class="border-grey" style="margin:auto;width:40%">
-        <p class="w3-large w3-center">35 days left</p>
-    </div>
-    <div class="display-bottom left padding-large">
-    </div>
-</div>
-</body>
 </html>
